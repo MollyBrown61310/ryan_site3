56e190d7765dcb78a762615ef2cd0146e9a7c42b
Molly Brown <mollybrown616@yahoo.com>
2025-07-11 14:03:48 -0400

    ...

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text", {
-    strings:["Musician" , "Songwriter" , "Artist" , "Instumentalist"],
+    strings:["Musician" , "Songwriter" , "Artist" ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
